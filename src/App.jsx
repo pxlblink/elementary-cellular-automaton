0a06baebd7962ebcbc2af6dcdc3ea4db6b81fe6c
pxlblink <249895803+pxlblink@users.noreply.github.com>
2025-12-23 18:49:37 +0300

    Move code to home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
-import { useState } from "react";
-import Grid from "./components/Grid";
-import Header from "./components/Header";
-import Input from "./components/Input";
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 
 function App() {
-  const [inputNo, setInputNo] = useState();
-  const [ruleNo, setRuleNo] = useState();
-  const [generated, setGenerated] = useState(false);
-
   return (
     <div>
       <Routes>
         <Route index element={<Home />} />
       </Routes>
-      <Header />
-      <Input
-        inputNo={inputNo}
-        setInputNo={setInputNo}
-        setRuleNo={setRuleNo}
-        setGenerated={setGenerated}
-      />
-      {generated ? (
-        <Grid ruleNo={ruleNo} columnSize={39} />
-      ) : (
-        <div className="flex justify-center my-10">
-          <p>Fill the input and press generate</p>
-        </div>
-      )}
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import Header from "../components/Header";
+import Input from "../components/Input";
+import Grid from "../components/Grid";
+
 export default function Home() {
-  return <p>hello world from home</p>;
+  const [inputNo, setInputNo] = useState();
+  const [ruleNo, setRuleNo] = useState();
+  const [generated, setGenerated] = useState(false);
+
+  return (
+    <>
+      <Header />
+      <Input
+        inputNo={inputNo}
+        setInputNo={setInputNo}
+        setRuleNo={setRuleNo}
+        setGenerated={setGenerated}
+      />
+      {generated ? (
+        <Grid ruleNo={ruleNo} columnSize={39} />
+      ) : (
+        <div className="flex justify-center my-10">
+          <p>Fill the input and press generate</p>
+        </div>
+      )}
+    </>
+  );
 }
