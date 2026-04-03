3c790935774398434f2c12f11822ddbe91203560
pxlblink <249895803+pxlblink@users.noreply.github.com>
2026-04-03 09:06:35 +0300

    Change text size to emphesize content higherarchy

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -8,25 +8,25 @@ export default function Help() {
       </h1>
       <div className="flex flex-col gap-2 justify-center content-center items-center px-3">
         <a
-          className="block text-xl text-center text-blue-800 underline text-balance"
+          className="block text-center text-blue-800 underline text-l text-balance"
           href="https://en.wikipedia.org/wiki/Elementary_cellular_automaton"
         >
           Wikipedia
         </a>
         <a
-          className="block text-xl text-center text-blue-800 underline text-balance"
+          className="block text-center text-blue-800 underline text-l text-balance"
           href="https://mathworld.wolfram.com/ElementaryCellularAutomaton.html"
         >
           Wolfram math world
         </a>
         <a
-          className="block text-xl text-center text-blue-800 underline text-balance"
+          className="block text-center text-blue-800 underline text-l text-balance"
           href="https://natureofcode.com/cellular-automata/"
         >
           Nature of code
         </a>
         <a
-          className="block text-xl text-center text-blue-800 underline text-balance"
+          className="block text-center text-blue-800 underline text-l text-balance"
           href="https://plato.stanford.edu/entries/cellular-automata/"
         >
           Stanford encyclopedia of philosophy
